b33c9324a0fee286ff1f08d58f08483c8b2e1966
Francisco Fernandez <fran6602@gmail.com>
2025-10-18 16:27:52 +0100

    feat: remove old method for:  authentication and tenant management controllers, routes, and services

--- src/routes/auth.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { loginHandler, registerHandler } from '../controllers/auth.controller.js';
-
-const router = Router();
-
-router.post('/login', loginHandler);
-router.post('/register', registerHandler);
-export default router;
